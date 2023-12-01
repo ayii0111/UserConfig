cf67c51b869593d0d02cb62988205ec4cb16517b
ayii <a0913873707@gmail.com>
2023-12-01 12:36:56 +0800

    重製前備份

---
+++ zsh_config/alias_script.zsh
@@ -0,0 +1,6 @@
+# zsh_hook 目錄
+# 將前次指令與執行結果儲存於 last_command、last_exit_code 全域變數
+source ~/UserScripts/zsh_hook/preec.zsh
+source ~/UserScripts/env.zsh
+source ~/UserScripts/initAlias.zsh
+# source ~/UserScripts/zle_tools/zle.zsh
